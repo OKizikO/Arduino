b0564a28b406f204176f974d3109ec47017d8666
AltKrypto <84981131+AltKrypto@users.noreply.github.com>
2022-07-30 11:29:57 -0400

    added variable for pincount
    added a variable to store the number of pins in use for looping

--- lights.ino
+++ lights.ino
@@ -17,9 +17,11 @@ int pin16 = A3;
 
 int allPins = {pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10, pin11, pin12, pin13, pin14, pin15, pin16};
 
+int pinCount = 16
+
 void setup(){
 
-for(int i=0;i<17;i++){
+for(int i=0;i<pinCount;i++){
     pinMode(allPins[i], OUTPUT);
 }
 
